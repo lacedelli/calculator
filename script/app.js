43f90575f5538d4f323ed09c27dc6917fbfb04cb
Lacedelli <lacedelli@outlook.com>
2019-07-27 17:53:31 -0500

    add multi-operator compatibility

--- script/app.js
+++ script/app.js
@@ -1,8 +1,7 @@
 const display = document.querySelector(".screen p");
 let operationString = "";
 
-// event listener that adds a number to the screen for
-// each number pressed
+// event listener for numpad buttons
 const numpad = document.querySelectorAll(".number");
 numpad.forEach((button) => { 
 	button.addEventListener("click", (event) => {
@@ -13,7 +12,7 @@ numpad.forEach((button) => {
 });
 	
 
-//TODO add an even listener for all the operator buttons
+// Event listener for all the operator buttons
 const operators = document.querySelectorAll(".operator");
 operators.forEach((button) => {
 	button.addEventListener("click", (event) => {
@@ -26,32 +25,15 @@ function clearDisplay(p){
 	operationString = "";
 }
 
+
 function processOperators(event) {
 	let operator = event.target.dataset.operation;
 	if(operator === "="){
-		// TODO add evaluation logic
 		let operationArray = operationString.split(" ");
-		console.log("split string into", operationArray);
-		let result = 0;
-		for(let i = 0; i < operationArray.length; i++){
-			let current = operationArray[i]; 
-			if(checkOperator(current)){
-				let operandA = Number(operationArray[i-1]);
-				let operandB = Number(operationArray[i+1]);
-				if(current === "+"){
-					result = operate(operandA,operandB,add);
-				} else if(current === "-") {
-					result = operate(operandA,operandB,subtract);
-				} else if(current === "*") {
-					result = operate(operandA,operandB,multiply);
-				} else if(current === "/") {
-					result = operate(operandA,operandB,divide);
-				}
-			} 
-			display.innerHTML = result;
-		}
-	//} else if() { // TODO add case for user trying to divide by zero
-		// TODO  
+		
+		operationString = solveExpression(operationArray);
+		display.innerHTML = operationString;
+		 
 	} else {
 		display.innerHTML += ` ${operator} `;
 		operationString += ` ${operator} `; 
@@ -61,6 +43,46 @@ function processOperators(event) {
 function checkOperator(tested){
 	if(tested === "+" || tested === "-" || tested === "*" || tested === "/"){
 		return true;
+	}
+}
+
+function solveExpression(operationArray){
+	
+	// If the array consists of only one value, it's done!
+	if(operationArray.length === 1){
+		return operationArray.toString();
+	}
+	for(let i = 0; i < operationArray.length; i++){
+		let current = operationArray[i]; 
+		// Checks current iteration is an operator
+		if(checkOperator(current)){
+			let operandA = Number(operationArray[i-1]);
+			let operandB = Number(operationArray[i+1]);
+			// for each case, solves operation, and creates new array
+			// with the remainder of the chain, inserting result at [0]
+			// recurses afterwards
+			if(current === "+"){
+				let result = operate(operandA,operandB,add);
+				let operated = operationArray.slice(i+2);
+				operated.unshift(result);
+				return solveExpression(operated);
+			} else if(current === "-") {
+				let result = operate(operandA,operandB,subtract);
+				let operated = operationArray.slice(i+2);
+				operated.unshift(result);
+				return solveExpression(operated);
+			} else if(current === "*") {
+				let result = operate(operandA,operandB,multiply);
+				let operated = operationArray.slice(i+2);
+				operated.unshift(result);
+				return solveExpression(operated);
+			} else if(current === "/") {
+				let result = operate(operandA,operandB,divide);
+				let operated = operationArray.slice(i+2);
+				operated.unshift(result);
+				return solveExpression(operated);
+			}
+		}
 	}
 }
 
